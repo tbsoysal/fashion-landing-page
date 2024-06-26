c90c0954ec4da378f35193326501b3fb047b313d
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-06-27 00:55:58 +0300

    Add all pages with styles

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+let menuItems = document.querySelectorAll('.navbar__links li')
+
+menuItems.forEach(li => {
+    li.classList.remove('active');
+});
+
+switch(window.location.pathname) {
+    case '/index.html':
+        menuItems[0].classList.add('active');
+    break;
+    case '/pages/collection.html':
+        menuItems[1].classList.add('active');
+    break;
+    case '/pages/parfume.html':
+        menuItems[2].classList.add('active');
+    break;
+    case '/pages/jewellery.html':
+        menuItems[3].classList.add('active');
+    break;
+    case '/pages/watchmaking.html':
+        menuItems[4].classList.add('active');
+    break;
+    case '/pages/about.html':
+        menuItems[5].classList.add('active');
+    break;
+}
+
+// Global Functions
 function redirectToCollection(){
     window.location.href = 'pages/collection.html';
 }
